9314f40e16ed1567826ab397390b3f1d6d857e2f
Maylon Magri <maylonmagri@gmail.com>
2018-05-26 13:36:09 -0300

    Criando as Classes ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/maylonmagri/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.maylonmagri.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maylonmagri.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+	
+}
