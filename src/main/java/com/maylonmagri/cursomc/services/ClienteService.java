0f0c4d3ab8101cb5caa772415b009ddcdce26430
Maylon Magri <maylonmagri@gmail.com>
2018-05-26 10:44:08 -0300

    Criando Endpoint /clientes/id

---
+++ src/main/java/com/maylonmagri/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.maylonmagri.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maylonmagri.cursomc.domain.Cliente;
+import com.maylonmagri.cursomc.repositories.ClienteRepository;
+import com.maylonmagri.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
