6c1133dffb15c25b0923fa5dc8d325c3eefa8e6f
Maylon Magri <maylonmagri@gmail.com>
2018-05-28 13:39:28 -0300

    Validação Sintática da Categoria com Bean Validation

--- src/main/java/com/maylonmagri/cursomc/services/CategoriaService.java
+++ src/main/java/com/maylonmagri/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.maylonmagri.cursomc.domain.Categoria;
+import com.maylonmagri.cursomc.dto.CategoriaDTO;
 import com.maylonmagri.cursomc.repositories.CategoriaRepository;
 import com.maylonmagri.cursomc.services.exceptions.DataIntegrityException;
 import com.maylonmagri.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
