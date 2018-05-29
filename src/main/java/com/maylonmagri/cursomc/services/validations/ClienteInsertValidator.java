0686204c2c9d38ea5105315103f99c59d0ce3225
Maylon Magri <maylonmagri@gmail.com>
2018-05-28 22:09:52 -0300

    Validação Customizada de Email já Cadastrado

--- src/main/java/com/maylonmagri/cursomc/services/validations/ClienteInsertValidator.java
+++ src/main/java/com/maylonmagri/cursomc/services/validations/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.maylonmagri.cursomc.domain.Cliente;
 import com.maylonmagri.cursomc.domain.enums.TipoCliente;
 import com.maylonmagri.cursomc.dto.ClienteNewDTO;
+import com.maylonmagri.cursomc.repositories.ClienteRepository;
 import com.maylonmagri.cursomc.resources.exceptions.FieldMessage;
 import com.maylonmagri.cursomc.services.validations.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -27,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existe"));
 		}
 
 		for (FieldMessage e : list) {
